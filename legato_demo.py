1ee8aeb78aa340203b13ea6c017e290d45aea71b
Pablo Veizaga <pablooliveiraveizaga@gmail.com>
2025-07-29 14:51:35 -0300

    Update legato_demo.py

--- legato_demo.py
+++ legato_demo.py
@@ -4,8 +4,8 @@ import pandas as pd
 import streamlit as st
 import os
 
-CLIENT_ID= ""
-CLIENT_SECRET = ""
+SPOTIPY_CLIENT_ID = ""
+SPOTIPY_CLIENT_SECRET = ""
 REDIRECT_URI = 'http://127.0.0.1:8001/callback'
 
 sp = spotipy.Spotify(
