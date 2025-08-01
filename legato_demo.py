63a7b79813a190c6161414c5d9dd445f360267c5
Pablo Veizaga <pablooliveiraveizaga@gmail.com>
2025-08-01 15:50:47 -0300

    Update legato_demo.py

--- legato_demo.py
+++ legato_demo.py
@@ -8,7 +8,7 @@ import base64
 # --- CONFIGURAÇÕES ---
 CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID", "")
 CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET", "")
-REDIRECT_URI = "https://legato-top10tracks.streamlit.app"
+REDIRECT_URI = "https://legato-top10tracks.streamlit.app/callback"
 
 # --- LINK DE AUTENTICAÇÃO ---
 params = {
